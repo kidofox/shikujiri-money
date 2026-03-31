ef8ccb3b14c9c905c4841256d7bafa93697a5a1d
kidofox <smafoxblog@gmail.com>
2026-04-01 01:22:24 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+import React from 'react';
+
+export const metadata = {
+  title: '人生逆転シミュレーター',
+  description: '仕組みでお金を稼ぐ',
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
       <body>{children}</body>
     </html>
-  )
+  );
 }
